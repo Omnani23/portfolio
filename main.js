876699229451c385e5101f32b66d6715a88e51b9
Nakka Om Prakash <98472529+Omnani23@users.noreply.github.com>
2023-12-01 20:31:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(
+    ".text", {
+        strings: ["Full Stack Developer", "Web developer", "App Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    }
+);
